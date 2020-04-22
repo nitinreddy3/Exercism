c818381dd4019a7be8e80275395015cdba211832
Nitin Reddy <reddynitin3@gmail.com>
2020-04-22 19:23:04 +0530

    Solved a resistor duo problem

--- dart/resistor-color-duo/lib/resistor_color_duo.dart
+++ dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,26 @@
 class ResistorColorDuo {
   // Put your code here
+  var colorCode = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+  };
+  var value;
+
+  ResistorColorDuo(List<String> value) {
+    this.value = value;
+  }
+
+  int result() {
+    var value1 = colorCode[value[0]];
+    var value2 = colorCode[value[1]];
+    return (10*value1) + value2;
+  }
 }
